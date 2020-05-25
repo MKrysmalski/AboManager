64edba6d24fac0e4bfdcb1995419dfb76908de94
Marvin2611 <64425349+Marvin2611@users.noreply.github.com>
2020-05-25 14:23:14 +0200

    Changes found under documentation

--- app/src/main/java/com/example/abomanager/MainActivity.kt
+++ app/src/main/java/com/example/abomanager/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.abomanager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
+import androidx.ui.foundation.Text
 import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
