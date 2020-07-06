e468792b7ff05bc345a1b56aa201f504571493c3
Marvin2611 <64425349+Marvin2611@users.noreply.github.com>
2020-07-06 12:49:59 +0200

    Marvin Commit 02. Added Buttons

--- app/src/main/java/com/example/abomanager/HomeScreen.kt
+++ app/src/main/java/com/example/abomanager/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.abomanager
 import androidx.compose.Composable
 import androidx.compose.remember
 import androidx.ui.foundation.Text
+import androidx.ui.material.Button
 import androidx.ui.material.Scaffold
 import androidx.ui.material.ScaffoldState
 
@@ -11,10 +12,39 @@ fun HomeScreen(scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
     Scaffold(
         scaffoldState = scaffoldState,
         topAppBar = {
+
             Text("Home Screen")
         },
         bodyContent = {
             Text("Empty Home Screen")
+        },
+        bottomAppBar = {
+            Button(onClick = {
+                Status.currentScreen = Screen.Statistics
+            }) {
+                Text("Statistics");
+            }
+            Button(onClick = {
+                Status.currentScreen = Screen.Statistics
+            }) {
+                Text("Navigate");
+            }
+            Button(onClick = {
+                Status.currentScreen = Screen.Home
+            }) {
+                Text("Home");
+            }
+            Button(onClick = {
+                Status.currentScreen = Screen.AboControl
+            }) {
+                Text("AboControl");
+            }
+            Button(onClick = {
+                Status.currentScreen = Screen.Profil
+            }) {
+                Text("Profil");
+            }
         }
+
     )
 }
