d183f01ef1bdf3fc291e2e48c9f97d0138b498e4
Marvin2611 <64425349+Marvin2611@users.noreply.github.com>
2020-07-07 12:11:13 +0200

    Marvin Commit 13. Weiter gehts

--- app/src/main/java/com/example/abomanager/AboControlEditScreen.kt
+++ app/src/main/java/com/example/abomanager/AboControlEditScreen.kt
@@ -2,19 +2,37 @@ package com.example.abomanager
 
 import androidx.compose.Composable
 import androidx.compose.remember
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
-import androidx.ui.material.Scaffold
-import androidx.ui.material.ScaffoldState
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.material.*
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.ArrowBack
+import androidx.ui.res.vectorResource
 
 @Composable
-fun AboControlEditScreen(scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
+fun AboControlEditScreen(viewModel: MainViewModel, scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
     Scaffold(
         scaffoldState = scaffoldState,
         topAppBar = {
-            Text("Abo Control Edit")
+            TopAppBar(
+                title = {
+                    Text(text = "Add Subscription")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        Status.currentScreen = Screen.AboControl(viewModel)
+                    }) {
+                        Icon(Icons.Filled.ArrowBack)
+                    }
+                }
+            )
         },
         bodyContent = {
-            Text("Empty Abo Control Edit")
+            Column {
+
+            }
         }
     )
 }
